7de126019ccb7ae9a2dee87a1d2acd7a85a99310
Valentina Conde <68123729+valentinaconde@users.noreply.github.com>
2025-06-30 18:11:09 -0300

    Reorganizar atributos en la clase SolicitudPrestamo para mejorar la estructura del código y facilitar la gestión de solicitudes de préstamo.

--- src/main/java/Dominio/SolicitudPrestamo.java
+++ src/main/java/Dominio/SolicitudPrestamo.java
@@ -3,9 +3,11 @@ package Dominio;
 public class SolicitudPrestamo {
     private int id_solicitud;
     private long dni_cliente;
-    private boolean autorizacion;
-    private int cuotas;
+    private long importe_solicitado;
     private long numero_cuenta_deposito;
+    private int cuotas;
+    private java.sql.Date fecha_solicitud;
+    private boolean autorizacion;
     private String estado;
 
     public int getId_solicitud() {
@@ -20,11 +22,17 @@ public class SolicitudPrestamo {
     public void setDni_cliente(long dni_cliente) {
         this.dni_cliente = dni_cliente;
     }
-    public boolean isAutorizacion() {
-        return autorizacion;
+    public long getImporte_solicitado() {
+        return importe_solicitado;
     }
-    public void setAutorizacion(boolean autorizacion) {
-        this.autorizacion = autorizacion;
+    public void setImporte_solicitado(long importe_solicitado) {
+        this.importe_solicitado = importe_solicitado;
+    }
+    public long getNumero_cuenta_deposito() {
+        return numero_cuenta_deposito;
+    }
+    public void setNumero_cuenta_deposito(long numero_cuenta_deposito) {
+        this.numero_cuenta_deposito = numero_cuenta_deposito;
     }
     public int getCuotas() {
         return cuotas;
@@ -32,11 +40,17 @@ public class SolicitudPrestamo {
     public void setCuotas(int cuotas) {
         this.cuotas = cuotas;
     }
-    public long getNumero_cuenta_deposito() {
-        return numero_cuenta_deposito;
+    public java.sql.Date getFecha_solicitud() {
+        return fecha_solicitud;
     }
-    public void setNumero_cuenta_deposito(long numero_cuenta_deposito) {
-        this.numero_cuenta_deposito = numero_cuenta_deposito;
+    public void setFecha_solicitud(java.sql.Date fecha_solicitud) {
+        this.fecha_solicitud = fecha_solicitud;
+    }
+    public boolean isAutorizacion() {
+        return autorizacion;
+    }
+    public void setAutorizacion(boolean autorizacion) {
+        this.autorizacion = autorizacion;
     }
     public String getEstado() {
         return estado;
